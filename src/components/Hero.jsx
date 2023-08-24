6f329047f4f9d605f066b254226c538e1b584cd1
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2023-08-24 12:39:01 -0700

    Added About section animation bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,14 +22,36 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 
           text-white-100`}>
-            I am a web developer <br 
-            className='sm:block hidden' /> interfaces
+            I develop web applications <br 
+            className='sm:block hidden' /> using javascript
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
-      <p>46:34</p>
+
+      <div className='absolute xs:bottom-10
+      bottom-32 w-full flex justify-center
+      items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] 
+          rounded-3xl border-4 border-secondary
+          flex justify-center items-start p-2'>
+            {/* Framer motion */}
+            <motion.div
+              animate={{
+                y:[0,24,0]
+              }}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:'loop'
+              }}
+              className='w-3 h-3 rounded-full
+              bg-secondary mb-1'/>
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -45,8 +45,8 @@ const ComputersCanvas = () => {
       camera = {{position: [20,3,5], fov:25 }}
       gl={{preserveDrawingBuffer:true }}
       >
-          <Suspense >
-        {/* <Suspense fallback={<CanvasLoader />}> */}
+         
+        <Suspense fallback={<CanvasLoader />}>
           <OrbitControls 
             enableZoom = {false}
             //rotate around a x axis
