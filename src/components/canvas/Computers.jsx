f5bd3f14796628d46e1194fa81b8645c3d2663f0
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2023-08-24 15:44:26 -0700

    Completed Hero component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,7 +8,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
     //import GLTF PC model
     const computer = useGLTF('./desktop_pc/scene.gltf')
 
@@ -29,8 +29,8 @@ const Computers = () => {
           />
         <primitive 
           object = {computer.scene}
-          scale = {0.75}
-          position = {[0,-3.25, -1.5]}
+          scale = {isMobile ? 0.7 : 0.75}
+          position = {isMobile ? [0, -3, -2.2]:[0,-3.25, -1.5]}
           rotation = {[-0.01, -0.2, -0.1]}
         />
       </mesh>
@@ -38,6 +38,31 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  //check if we are on a mobile device
+  useEffect (() =>{
+    //Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia(
+    ('max-width:500px'))
+    
+    //Set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+
+    //Define a callback function to handle changes to media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    //Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    //Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
   return(
     <Canvas
       frameloop='demand'
@@ -49,12 +74,12 @@ const ComputersCanvas = () => {
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls 
             enableZoom = {false}
-            //rotate around a x axis
+            //rotate the model around x axis
             maxPolarAngle={Math.PI / 2 }
             minPolarAngle={Math.PI / 2 } 
             />
             <mesh>
-              <Computers />
+              <Computers isMobile={isMobile}/>
             </mesh>
         </Suspense>
         <Preload all />
