2f9568aa16acdfa778ce09f3e5bb8dfb652e114c
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-02-13 12:34:05 -0700

    updated path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: { chunkSizeWarningLimit: 1600, },
-  base: "/portfolio/",
+  base: "https://spikey2023.github.io/portfolio/",
 })
