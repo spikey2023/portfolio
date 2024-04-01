226e63a4c53c63d02b647f58f97bda44d10d0d96
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-03-31 19:44:05 -0700

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center p-5 text-light">
+            © 2024 Choudhury Asif Ibne Yusuf
+          </div>
+  )
+}
+
+export default Footer
